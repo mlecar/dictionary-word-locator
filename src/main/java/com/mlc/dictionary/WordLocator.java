8939835413fe61767804f901b32b6581002ce89e
Marcelo Lecar <marcelo.lecar@gmail.com>
2018-04-23 07:02:37 -0300

    Initial commit

---
+++ src/main/java/com/mlc/dictionary/WordLocator.java
@@ -0,0 +1,18 @@
+package com.mlc.dictionary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WordLocator implements CommandLineRunner {
+
+    @Autowired
+    private FileReader fileReader;
+
+    @Override
+    public void run(String... args) throws Exception {
+        fileReader.readFile();
+    }
+
+}
